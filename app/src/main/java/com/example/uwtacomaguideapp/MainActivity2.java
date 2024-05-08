31f4b1ceb9c3c654dd8ab3de599c49b9cec904e0
gabraltar <chojustin0609@gmail.com>
2024-05-08 13:38:43 -0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- app/src/main/java/com/example/uwtacomaguideapp/MainActivity2.java
+++ app/src/main/java/com/example/uwtacomaguideapp/MainActivity2.java
@@ -80,7 +80,7 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
             TextView textview2 = findViewById(R.id.textView2);
             String lat = String.valueOf(location.getLatitude());
             String lng = String.valueOf(location.getLongitude());
-            ImageView imageView = findViewById(R.id.imageView3);
+            //ImageView imageView = findViewById(R.id.imageView3);
             textview1.setText(lat);
             textview2.setText(lng);
             //imageView.setX(0);
@@ -120,7 +120,7 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
 //        TextView textview1 = findViewById(R.id.textView);
 //        TextView textview2 = findViewById(R.id.textView2);
         ImageView imageView = findViewById(R.id.imageView3);
-        ImageView imageView1 = findViewById(R.id.imageView);
+        //ImageView imageView1 = findViewById(R.id.imageView);
         double lat1 = location.getLatitude();
         double lng1 = location.getLongitude();
         String lat = String.valueOf(lat1);
