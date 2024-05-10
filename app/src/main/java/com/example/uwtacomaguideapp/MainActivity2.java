3379f85cb66cc0bab780b1cb5b524acba6db37b9
gabraltar <chojustin0609@gmail.com>
2024-05-10 07:52:04 -0700

    adjusting coords for new map

--- app/src/main/java/com/example/uwtacomaguideapp/MainActivity2.java
+++ app/src/main/java/com/example/uwtacomaguideapp/MainActivity2.java
@@ -145,14 +145,14 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
         //47.24676, -122.44121 | 47.24676 -122.43613 | 47.24258, -122.44121 | 47.24258, -122.43613
         // 0.00418, -0.00508
         //432, 376
-        if((lng1 >= -122.44121 && lng1 <= -122.43613) && (lat1 <= 47.24676 && lat1 >= 47.24258)){
+        if((lng1 >= -122.44131 && lng1 <= -122.43294) && (lat1 <= 47.24844 && lat1 >= 47.24258)){
             //lat1 = Math.floor(2.2 * ((lat1 - 47.24258) * 100000));
             //lng1 = Math.floor(2.26 * ((lng1 - (-122.44121)) * 100000));
             //imageView.setX(((float)lat1));
             //imageView.setY(((float)lng1)); //383
 
-            lat1 = Math.floor( 1220 * (1 - (((lat1 - 47.24258) * 100000) / 418)));
-            lng1 = Math.floor(1000 * (((lng1 - (-122.44121)) * 100000) / 508));
+            lat1 = Math.floor( 1220 * (1 - (((lat1 - 47.24258) * 100000) / 586)));
+            lng1 = Math.floor(1000 * (((lng1 - (-122.44121)) * 100000) / 837));
             //textview3.setText(height);
             //textview4.setText(width);
             //textview1.setText(lat);
