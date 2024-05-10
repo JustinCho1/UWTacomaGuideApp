1383689568bfdad16068f3d21cd89c280525fa65
gabraltar <chojustin0609@gmail.com>
2024-05-10 10:58:44 -0700

    adjusting coords for new map

--- app/src/main/java/com/example/uwtacomaguideapp/MainActivity2.java
+++ app/src/main/java/com/example/uwtacomaguideapp/MainActivity2.java
@@ -168,8 +168,8 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
             //imageView.setX(((float)lat1));
             //imageView.setY(((float)lng1)); //383
 
-            lat1 = Math.floor(width * ((((lat1 - 47.24258) * 100000) / 825)));
-            lng1 = Math.floor((width) * (((lng1 - (-122.44131)) * 100000) / 800));
+            lat1 = Math.floor(width * ((((lat1 - 47.24258) * 100000) / 900)));
+            lng1 = Math.floor((width) * (((lng1 - (-122.44131)) * 100000) / 900));
             //textview3.setText(height);
             //textview4.setText(width);
             //textview1.setText(lat);
