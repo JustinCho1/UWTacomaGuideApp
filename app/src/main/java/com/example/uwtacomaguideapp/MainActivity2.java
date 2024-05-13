f46ad331e9b9e31328dffd77bf61c76acb887d0a
gabraltar <chojustin0609@gmail.com>
2024-05-13 10:01:38 -0700

    prayge

--- app/src/main/java/com/example/uwtacomaguideapp/MainActivity2.java
+++ app/src/main/java/com/example/uwtacomaguideapp/MainActivity2.java
@@ -29,7 +29,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 public class MainActivity2 extends AppCompatActivity implements LocationListener {
-    Button button1;
+    private Button button1;
+    private ImageView pointer;
+    private double width;
     private FusedLocationProviderClient fusedLocationClient;
     public static final String GPS_PROVIDER = "gps";
     //private final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -72,7 +74,7 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
         }
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        double width = displayMetrics.widthPixels;
+        width = displayMetrics.widthPixels;
         ImageView map = findViewById(R.id.imageView3);
         Location location = locationManager.getLastKnownLocation("gps");
         //LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -135,19 +137,6 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
         }
         setContentView(R.layout.activity_main2);
 
-        //imageView1.setImageMatrix()
-        button1 = (Button) findViewById(R.id.button2);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-//        map = findViewById(R.id.imageView3);
-        //imageView.setY((float)height);
-        //Log.i(String.valueOf(width), "width is: ");;
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates("gps", 400, 0.1f, this);
 
@@ -156,55 +145,14 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
     public void onLocationChanged(@NonNull Location location) {
 
         setContentView(R.layout.activity_main2);
-        button1 = (Button) findViewById(R.id.button2);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        double width = displayMetrics.widthPixels;
-        //TextView textview1 = findViewById(R.id.textView);
-        //TextView textview2 = findViewById(R.id.textView2);
-        //TextView textview3 = findViewById(R.id.textView3);
-        //TextView textview4 = findViewById(R.id.textView4);
-        //TextView textview1 = findViewById(R.id.textView);
-        //TextView textview2 = findViewById(R.id.textView2);
-        ImageView imageView = findViewById(R.id.imageView);
-
-        //int screenHeight = displayMetrics.heightPixels;
-
-        //Log.d(String.valueOf(screenWidth), "width is: ");
-        //Log.d(String.valueOf(width), "height is: ");
         double lat1 = location.getLatitude();
         double lng1 = location.getLongitude();
 
-        //String lat = String.valueOf(lat1);
-        //String lng = String.valueOf(lng1);
-
-        //47.24676, -122.44121 | 47.24676 -122.43613 | 47.24258, -122.44121 | 47.24258, -122.43613
-        // 0.00418, -0.00508
-        //432, 376
         if((lng1 >= -122.44302 && lng1 <= -122.43257) && (lat1 <= 47.24818 && lat1 >= 47.24315)){
-            //lat1 = Math.floor(2.2 * ((lat1 - 47.24258) * 100000));
-            //lng1 = Math.floor(2.26 * ((lng1 - (-122.44121)) * 100000));
-            //imageView.setX(((float)lat1));
-            //imageView.setY(((float)lng1)); //383
-
             lat1 = Math.floor(width * (0.583) * (1 - (((lat1 - 47.24315) * 100000) / 503))); //937
             lng1 = Math.floor((width) * (((lng1 - (-122.44271)) * 100000) / 1045)); //767
-            //textview3.setText(height);
-            //textview4.setText(width);
-            //textview1.setText(lat);
-            //textview2.setText(lng);
-            imageView.setX((float)lng1);
-            imageView.setY((float)lat1);
-            //textview1.setText(String.valueOf(location.getLatitude() + " " + location.getLongitude()));
-            //textview2.setText(String.valueOf(lat1 + " " + lng1));
-            //imageView.setY(2500);
+            pointer.setX((float)lng1);
+            pointer.setY((float)lat1);
 
         }
     }
