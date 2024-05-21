339ec6763216b4deaafbfd87870ff3e3cee82875
Michael <michaelttra@gmail.com>
2024-05-20 23:44:09 -0700

    Clipboard works for Contacts Page

--- app/src/main/java/com/example/uwtacomaguideapp/contactsActivity.java
+++ app/src/main/java/com/example/uwtacomaguideapp/contactsActivity.java
@@ -32,8 +32,40 @@ public class contactsActivity extends AppCompatActivity {
 
         button1 = (Button) findViewById(R.id.homeContacts);
 
+        // General
         Button genPhone1 = findViewById(R.id.buttonGenPhone1);
-        final String phoneNumber = "253-692-4000";
+
+        Button genPhone2 = findViewById(R.id.buttonGenPhone2);
+
+        Button genEmail = findViewById(R.id.buttonGenEmail);
+
+        // Admissions
+        Button admissionPhone = findViewById(R.id.buttonAdmissionPhone);
+
+        Button admissionEmail = findViewById(R.id.buttonAdmissionEmail);
+
+        // Financial
+        Button financialPhone = findViewById(R.id.buttonFinancialPhone);
+
+        Button financialEmail = findViewById(R.id.buttonFinancialEmail);
+
+        // Office
+        Button officePhone = findViewById(R.id.buttonOfficePhone);
+
+        Button officeEmail = findViewById(R.id.buttonOfficeEmail);
+
+        // IT
+        Button ITEmail = findViewById(R.id.buttonITEmail);
+
+        // Alumni
+        Button alumniPhone = findViewById(R.id.buttonAlumniPhone);
+
+        Button alumniEmail = findViewById(R.id.buttonAlumniEmail);
+
+        // Alumni
+        Button safetyPhone = findViewById(R.id.buttonSafetyPhone);
+
+        Button safetyEmail = findViewById(R.id.buttonSafetyEmail);
 
 
         //TextView textview3 = findViewById(R.id.textView3);
@@ -44,15 +76,38 @@ public class contactsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        genPhone1.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener copyTextListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Button button = (Button) v;
+                String buttonText = button.getText().toString();
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("Phone Number", phoneNumber);
+                ClipData clip = ClipData.newPlainText("Button Text", buttonText);
                 clipboard.setPrimaryClip(clip);
 
-                Toast.makeText(contactsActivity.this, "Copied Number to Clipboard", Toast.LENGTH_SHORT).show();
+                Toast.makeText(contactsActivity.this, "Copied to Clipboard", Toast.LENGTH_SHORT).show();
             }
-        });
+        };
+
+        genPhone1.setOnClickListener(copyTextListener);
+        genPhone2.setOnClickListener(copyTextListener);
+        genEmail.setOnClickListener(copyTextListener);
+
+        admissionPhone.setOnClickListener(copyTextListener);
+        admissionEmail.setOnClickListener(copyTextListener);
+
+        financialPhone.setOnClickListener(copyTextListener);
+        financialEmail.setOnClickListener(copyTextListener);
+
+        officePhone.setOnClickListener(copyTextListener);
+        officeEmail.setOnClickListener(copyTextListener);
+
+        ITEmail.setOnClickListener(copyTextListener);
+
+        alumniPhone.setOnClickListener(copyTextListener);
+        alumniEmail.setOnClickListener(copyTextListener);
+
+        safetyPhone.setOnClickListener(copyTextListener);
+        safetyEmail.setOnClickListener(copyTextListener);
     }
 }
