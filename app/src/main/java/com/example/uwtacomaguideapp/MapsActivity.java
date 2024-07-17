b15223ed4b4140497bfcce486f7d03feb9710e5f
JustinCho1 <icebeam891@gmail.com>
2024-07-17 01:50:25 -0700

    overhauled map page, testing zoom features

--- app/src/main/java/com/example/uwtacomaguideapp/MapsActivity.java
+++ app/src/main/java/com/example/uwtacomaguideapp/MapsActivity.java
@@ -1,17 +1,25 @@
 package com.example.uwtacomaguideapp;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -21,14 +29,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-
+    private FusedLocationProviderClient fusedLocationClient;
+    private GoogleMap globalGoogleMap;
+    public static final String GPS_PROVIDER = "gps";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         EdgeToEdge.enable(this);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -38,7 +51,32 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        AtomicBoolean permissions = new AtomicBoolean(false);
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    android.Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    android.Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                permissions.set(true);
+                                // Precise location access granted.
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                permissions.set(false);
+                                // Only approximate location access granted.
+                            } else {
+                                permissions.set(false);
+                                // No location access granted.
 
+                            }
+                        }
+                );
+
+        locationPermissionRequest.launch(new String[] {
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+        });
 
 
 
@@ -47,11 +85,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        LatLng uwt = new LatLng(47.245590715390174, -122.43912890988082);
-        GroundOverlayOptions uwtmap = new GroundOverlayOptions()
-                .image(BitmapDescriptorFactory.fromResource(R.drawable.uwtmap))
-                        .position(uwt, 753f, 953f);
-
-        googleMap.addGroundOverlay(uwtmap);
+        globalGoogleMap = googleMap;
     }
+
+    public void onLocationChanged(@NonNull Location location) {
+        double lat1 = location.getLatitude();
+        double lng1 = location.getLongitude();
+        LatLng sydney = new LatLng(lat1, lng1);
+
+        globalGoogleMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+
+    }
+
 }
