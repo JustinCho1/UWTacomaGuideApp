3a270ab9035cb2f945678f5e6ade11144771c098
JustinCho1 <icebeam891@gmail.com>
2024-07-23 15:55:47 -0700

    added marker that moves

--- app/src/main/java/com/example/uwtacomaguideapp/MapsActivity.java
+++ app/src/main/java/com/example/uwtacomaguideapp/MapsActivity.java
@@ -1,9 +1,12 @@
 package com.example.uwtacomaguideapp;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,14 +29,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.File;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
     private FusedLocationProviderClient fusedLocationClient;
     private GoogleMap globalGoogleMap;
+    private Marker m;
     public static final String GPS_PROVIDER = "gps";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,9 +61,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 registerForActivityResult(new ActivityResultContracts
                                 .RequestMultiplePermissions(), result -> {
                             Boolean fineLocationGranted = result.getOrDefault(
-                                    android.Manifest.permission.ACCESS_FINE_LOCATION, false);
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
                             Boolean coarseLocationGranted = result.getOrDefault(
-                                    android.Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
                             if (fineLocationGranted != null && fineLocationGranted) {
                                 permissions.set(true);
                                 // Precise location access granted.
@@ -74,11 +79,37 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 );
 
         locationPermissionRequest.launch(new String[] {
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
         });
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        locationManager.requestLocationUpdates("gps", 400, 0.1f, this);
+
+
+    }
+    protected void onResume() {
+        super.onResume();
+
+    }
+    public void onLocationChanged(@NonNull Location location) {
+
+        double lat1 = location.getLatitude();
+        double lng1 = location.getLongitude();
+        LatLng sydney = new LatLng(lat1, lng1);
+        m.setPosition(sydney);
 
 
     }
@@ -86,17 +117,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         globalGoogleMap = googleMap;
-    }
-
-    public void onLocationChanged(@NonNull Location location) {
-        double lat1 = location.getLatitude();
-        double lng1 = location.getLongitude();
+        double lat1 = 47.24558;
+        double lng1 = -122.43801;
         LatLng sydney = new LatLng(lat1, lng1);
-
-        globalGoogleMap.addMarker(new MarkerOptions()
+        m = globalGoogleMap.addMarker(new MarkerOptions()
                 .position(sydney)
                 .title("Marker in Sydney"));
-
     }
+
+
 
 }
