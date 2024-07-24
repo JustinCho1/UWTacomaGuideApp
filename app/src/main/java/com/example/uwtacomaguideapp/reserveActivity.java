00e247491999bbe079235a746be132e49224204b
cchan <chunchan137@gmail.com>
2024-07-24 12:01:19 -0700

    Embedded the 25live tutorial video, made the rest of the text readable in dark mode. Deleted screenshots from the page and drawable folder.

--- app/src/main/java/com/example/uwtacomaguideapp/reserveActivity.java
+++ app/src/main/java/com/example/uwtacomaguideapp/reserveActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
@@ -52,21 +54,13 @@ public class reserveActivity extends AppCompatActivity {
                 goToUrl("https://www.tacoma.uw.edu/uuf/study-spaces");
             }
         });
-        VideoView videoView = findViewById(R.id.videoView);
 
-        Uri uri = Uri.parse(videoUrl);
+        WebView webView = findViewById(R.id.webView);
+        String video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/SIukzM1kBA8?si=ZvAstJeWzFoUPK7Z\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+        webView.loadData(video, "text/html", "utf-8");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
 
-        videoView.setVideoURI(uri);
-
-        MediaController mediaController = new MediaController(this);
-
-        mediaController.setAnchorView(videoView);
-
-        mediaController.setMediaPlayer(videoView);
-
-        videoView.setMediaController(mediaController);
-
-        videoView.start();
     }
 
     private void goToUrl(String url) {
